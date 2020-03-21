2d205045f8fdb71aa2c53377da5e2354ed852c72
Reinier van den Assum <rvandenassum@deloitte.nl>
2020-03-21 18:04:08 +0100

    2020-03-21: EmailVitality Outlook Add-in
    * Enables a Ribbon Tab 'Email Vitality WorkingHours' in Mail Composer screen
    * Ribbon input is validated and stored in Application Settings (for re-use)
    * On Email Send validates whether current time is outside defined Work Hours
    * When within working hours, email is directly send
    * When outside working hours, user is (optionally) prompted whether or not to postpone email

---
+++ vba/outlook-addins/EmailVitality/EmailVitality.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.Office.Interop.Outlook
+
+Public Class EmailVitality
+
+    Private Sub EmailVitality_Startup() Handles Me.Startup
+    End Sub
+
+    Private Sub EmailVitality_Shutdown() Handles Me.Shutdown
+    End Sub
+
+    Private Sub Application_ItemSend(Item As Object, ByRef Cancel As Boolean) Handles Application.ItemSend
+        Dim objMail As MailItem
+
+        If TypeOf Item Is MailItem Then
+            objMail = Item
+
+            ' Fetch user input from Ribbon (Start and End times of WorkingDay & to always postpone delivery)
+            Dim WorkingDayStart As String = Globals.Ribbons.EmailVitalityRibbon.WorkHoursStart.Text
+            Dim WorkingDayEnd As String = Globals.Ribbons.EmailVitalityRibbon.WorkHoursEnd.Text
+            Dim bShowPrompt As Boolean = Globals.Ribbons.EmailVitalityRibbon.ShowPrompt.Checked
+
+            Dim today As Date = Date.Now().Date
+            Dim bDelayMail As Boolean = True
+            Dim NewSendTime As String
+            Dim nPrompt As Integer
+
+            Select Case Weekday(today, vbMonday)
+                Case 6 'Saturday - Delay 2 days
+                    NewSendTime = CombineDateTime(today.AddDays(2), WorkingDayStart)
+                Case 7 'Sunday - Delay 1 day
+                    NewSendTime = CombineDateTime(today.AddDays(1), WorkingDayStart)
+                Case Else 'WeekDay - Only Delay when outside WorkingHours
+                    If Now < CombineDateTime(today, WorkingDayStart) Then 'Before WorkingDay
+                        NewSendTime = CombineDateTime(today, WorkingDayStart)
+                    ElseIf Now > CombineDateTime(today, WorkingDayEnd) Then 'After WorkingDay
+                        'Set NewSendTime based on Weekday
+                        Select Case Weekday(today, vbMonday)
+                            Case 5 'Friday - Delay 3 days
+                                NewSendTime = CombineDateTime(today.AddDays(3), WorkingDayStart)
+                            Case Else 'Else - Delay till tomorrow morning
+                                NewSendTime = CombineDateTime(today.AddDays(1), WorkingDayStart)
+                        End Select
+                    Else
+                        bDelayMail = False
+                    End If
+            End Select
+
+            'When email should be delayed (NOW is after Business Hours) & Email is not yet delayed
+            If bDelayMail = True And objMail.DeferredDeliveryTime = "1/1/4501" Then
+                Dim NewDateTime As Date = NewSendTime
+                If bShowPrompt = True Then
+                    'Ask if to delay sending this email
+                    nPrompt = MsgBox("Uh-oh.. it's outside Working Hours:" & vbCrLf & vbCrLf & "Click YES to DELAY (" & WeekdayName(Weekday(NewDateTime, vbMonday), True, vbMonday) & " " & NewSendTime & ")." & vbCrLf & vbCrLf & "Click NO to SEND NOW, when:" & vbCrLf & "- Email is so urgent, colleagues MUST read it now." & vbCrLf & "- You expect a reply BEFORE start of next Working Hours." & vbCrLf & vbCrLf & "Click CANCEL to not send at all.", vbYesNoCancel + vbCritical, "Delay Email")
+
+                    If nPrompt = vbYes Then 'Delay email
+                        objMail.DeferredDeliveryTime = NewSendTime
+                    ElseIf nPrompt = vbNo Then 'Send directly
+                        objMail.DeferredDeliveryTime = "1/1/4501"
+                    Else 'Send Operation Cancelled
+                        Cancel = True
+                    End If
+                Else
+                    ' Skip prompt and automatically set the NewSendDtime
+                    objMail.DeferredDeliveryTime = NewSendTime
+                    MsgBox("Email rescheduled to be delivered at " & WeekdayName(Weekday(NewDateTime, vbMonday), True, vbMonday) & " " & NewSendTime)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Function CombineDateTime(DateString As String, TimeString As String)
+        CombineDateTime = DateString & " " & TimeString
+    End Function
+
+End Class
---
+++ vba/outlook-addins/EmailVitality/EmailVitalityRibbon.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Text.RegularExpressions
+Imports Microsoft.Office.Tools.Ribbon
+
+Public Class EmailVitalityRibbon
+
+    Public Function ValidateTime(ByVal strTime As String) As Boolean
+        ' Check whether not empty
+        If Not String.IsNullOrEmpty(strTime) Then
+            ' Setup Regex (Regular Expression)
+            Dim strTimePattern As String = "^\d{2}:\d{2}:\d{2}$"
+            Dim rePhone As New Regex(strTimePattern)
+
+            Return rePhone.IsMatch(strTime) 'Check Validity
+        Else
+            Return False 'Empty, thus not valid
+        End If
+    End Function
+
+    Private Sub WorkHoursStart_TextChanged(sender As Object, e As RibbonControlEventArgs) Handles WorkHoursStart.TextChanged
+        If Not ValidateTime(WorkHoursStart.Text) Then
+            MsgBox("Invalid input provided " & WorkHoursStart.Text & ". Reset to: 08:30:00.")
+            WorkHoursStart.Text = "08:30:00"
+        Else
+            ' When valid new input, update Application Setting
+            My.Settings.WorkHoursStart = WorkHoursStart.Text
+        End If
+    End Sub
+
+    Private Sub WorkingHoursEnd_TextChanged(sender As Object, e As RibbonControlEventArgs) Handles WorkHoursEnd.TextChanged
+        If Not ValidateTime(WorkHoursEnd.Text) Then
+            MsgBox("Invalid input provided " & WorkHoursStart.Text & ". Reset to: 17:00:00.")
+            WorkHoursEnd.Text = "17:00:00"
+        Else
+            ' When valid new input, update Application Setting
+            My.Settings.WorkHoursEnd = WorkHoursEnd.Text
+        End If
+    End Sub
+End Class
